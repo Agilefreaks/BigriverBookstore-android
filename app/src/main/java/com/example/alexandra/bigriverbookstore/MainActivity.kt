cd34248ce73c2c36f32fea8590de9d90a36c111d
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-05-02 10:59:20 +0300

    The RecyclerView and Layout Manager.

--- app/src/main/java/com/example/alexandra/bigriverbookstore/MainActivity.kt
+++ app/src/main/java/com/example/alexandra/bigriverbookstore/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.alexandra.bigriverbookstore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+
     }
 }
