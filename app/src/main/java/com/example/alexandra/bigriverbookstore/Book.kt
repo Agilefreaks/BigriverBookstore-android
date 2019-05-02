568983d41c7a56daf73c69d4a03f230b41bba649
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-05-02 14:04:59 +0300

    display basic list with name and title

---
+++ app/src/main/java/com/example/alexandra/bigriverbookstore/Book.kt
@@ -0,0 +1,3 @@
+package com.example.alexandra.bigriverbookstore
+
+data class User(val name: String, val title: String)
--- app/src/main/java/com/example/alexandra/bigriverbookstore/MainActivity.kt
+++ app/src/main/java/com/example/alexandra/bigriverbookstore/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.alexandra.bigriverbookstore
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rv = findViewById<RecyclerView>(R.id.book_list)
+        rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        val users = ArrayList<User>()
+        users.add(User("Paul", "book t1"))
+        users.add(User("Jane", "book t2"))
+        users.add(User("John", "book t3"))
+        users.add(User("Amy", "book t4"))
+
+        var adapter = BookAdapter(users)
+        rv.adapter = adapter
     }
 }
