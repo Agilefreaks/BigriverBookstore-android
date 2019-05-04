3e57e70951a62faeef8565a6555f3975fe6acef0
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-05-04 23:28:23 +0300

    The getContext is replaced by getDocument for clarity in 3.3.0.

--- app/src/main/java/com/example/alexandra/bigriverbookstore/javaModels/Books.java
+++ app/src/main/java/com/example/alexandra/bigriverbookstore/javaModels/Books.java
@@ -1,16 +1,27 @@
 package com.example.alexandra.bigriverbookstore.javaModels;
 
 import com.squareup.moshi.Json;
+import moe.banana.jsonapi2.HasMany;
+import moe.banana.jsonapi2.HasOne;
 import moe.banana.jsonapi2.JsonApi;
+import moe.banana.jsonapi2.Resource;
 
 import java.util.List;
 
 @JsonApi(type = "books")
-public class Books {
+public class Books extends Resource {
     public String title;
     @Json(name = "date_published")
     public String datePublished;
     public String isbn;
-    public String author;
-    public List<Photos> photos;
+    public HasOne<Authors> author;
+    public HasMany<Photos> photos;
+
+    public Authors getAuthor() {
+        return author.get(getDocument());
+    }
+
+    public List<Photos> getAllPhotos() {
+        return photos.get(getDocument());
+    }
 }
