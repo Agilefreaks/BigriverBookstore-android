a8b874802dca3d6766bac0f9fe8794ea9bc5f84d
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-05-04 22:55:43 +0300

    Expect class out resource error

---
+++ app/src/main/java/com/example/alexandra/bigriverbookstore/ApiRequests.kt
@@ -0,0 +1,50 @@
+package com.example.alexandra.bigriverbookstore
+
+import com.example.alexandra.bigriverbookstore.javaModels.Books
+import com.example.alexandra.bigriverbookstore.javaModels.Photos
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import moe.banana.jsonapi2.JsonApiConverterFactory
+import moe.banana.jsonapi2.ResourceAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
+//https://medium.com/@r4rohit002/parse-json-with-moshi-873d0ff6af9b
+//https://github.com/kamikat/moshi-jsonapi
+
+interface ApiRequests {
+    @GET("books?include=photos,author")
+    fun getBooks(): Call<Books>
+
+    companion object {
+        fun getMoshi(): Moshi {
+            // First create the factory
+            val jsonApiAdapterFactory = ResourceAdapterFactory.builder()
+                .add(Books::class.java)
+                .add(Photos::class.java)
+                .build()
+
+            // Create a custom moshi instance
+            val moshi = Moshi.Builder()
+                .add(jsonApiAdapterFactory)
+                .build()
+            return moshi;
+        }
+
+        fun newInstance(): ApiRequests {
+            val moshi = getMoshi()
+            val client = OkHttpClient().newBuilder()
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://jsonapiplayground.reyesoft.com/v2/")
+                .client(client)
+                .addConverterFactory(JsonApiConverterFactory.create(moshi))
+                .build()
+
+            return retrofit.create(ApiRequests::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/alexandra/bigriverbookstore/BooksRepo.kt
@@ -0,0 +1,34 @@
+package com.example.alexandra.bigriverbookstore
+
+import com.example.alexandra.bigriverbookstore.javaModels.Books
+import moe.banana.jsonapi2.Document
+import retrofit2.Response
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+
+class BooksRepository(
+    private val api: ApiRequests,
+    private val executor: ExecutorService = Executors.newSingleThreadExecutor()
+) : BooksReturns {
+
+    class ErrorException(error: Any?) : RuntimeException("Error: $error")
+
+    override fun getBooks(): Future<List<Book>> {
+        return executor.submit<List<Book>> {
+            val response = api.getBooks().execute()
+            val body = verifyResponse(response)
+            val books = body.toList()
+            books.map { Book.from(it) }
+        }
+    }
+
+    private fun verifyResponse(response: Response<Document<Books>>): Document<Books> {
+        val statusCode = response.code()
+        if (statusCode != 200) {
+            throw  ErrorException("Status code different fro 200. Error body: ${response.errorBody().toString()}")
+        }
+        return response.body()
+            ?: throw ErrorException("Null body found. Error body: ${response.errorBody().toString()}")
+    }
+}
