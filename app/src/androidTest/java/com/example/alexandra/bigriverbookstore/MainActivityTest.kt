82e055e9b525913950d93dc81a10a0c6f112148a
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-05-06 12:53:16 +0300

    [#8] Test if first item in list has title and author

--- app/src/androidTest/java/com/example/alexandra/bigriverbookstore/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/alexandra/bigriverbookstore/MainActivityTest.kt
@@ -1,12 +1,15 @@
 package com.example.alexandra.bigriverbookstore
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
+import com.example.alexandra.bigriverbookstore.CustomAssertions.Companion.hasItemCount
+import com.example.alexandra.bigriverbookstore.CustomMatchers.Companion.withItemCount
 import org.junit.Rule
 import org.junit.Test
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import com.example.alexandra.bigriverbookstore.CustomMatchers.Companion.withItemCount
 
 class MainActivityTest {
     @Rule
@@ -22,8 +25,27 @@ class MainActivityTest {
     }
 
     @Test
-    fun checkFirstItemInListExists() {
+    fun countBooksWithViewAssertion() {
+        onView(withId(R.id.book_list))
+            .check(hasItemCount(6))
+    }
+
+    @Test
+    fun checkIfFirstItemInListExists() {
         // write a test that will check that the first item in the recycler
         // view has a title and a author name
+        onView(
+            RecyclerViewMatcher(R.id.book_list)
+                .atPositionOnView(0, R.id.txtName)
+        )
+            .check(matches(withText("Paul k")))
+            .perform(click())
+
+        onView(
+            RecyclerViewMatcher(R.id.book_list)
+                .atPositionOnView(0, R.id.txtTitle)
+        )
+            .check(matches(withText("book t1")))
+            .perform(click())
     }
 }
