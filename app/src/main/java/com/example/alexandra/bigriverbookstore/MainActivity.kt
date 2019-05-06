3976eac0ffab79992e54329505ad0b119721eeb5
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-05-06 15:56:58 +0300

    [#8]  Use data binding library

--- app/src/main/java/com/example/alexandra/bigriverbookstore/MainActivity.kt
+++ app/src/main/java/com/example/alexandra/bigriverbookstore/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.alexandra.bigriverbookstore
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
+import com.example.alexandra.bigriverbookstore.databinding.BookListItemBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding: BookListItemBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val bookModel = Book("Author name", "Book title", "https://lorempixel.com//400//300//abstract//Faker//?50138")
+        binding.setVariable(BR.bookModel, bookModel)
+        binding.setLifecycleOwner(this)
+        binding.executePendingBindings()
 
         val rv = findViewById<RecyclerView>(R.id.book_list)
         rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
