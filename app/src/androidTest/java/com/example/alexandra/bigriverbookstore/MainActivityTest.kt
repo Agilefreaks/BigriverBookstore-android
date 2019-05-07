a9009cea81c1ec23c717cb4f6bcdc81892eb35d6
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2019-05-07 11:18:18 +0300

    Feature/8 main screen (#4)
    * initial project
    
    * clean up
    
    * The RecyclerView and Layout Manager.
    
    * Recycle view item row created
    
    * Recycle adapter created
    
    * photo activity
    
    * basic UI
    
    * display basic list with name and title
    
    * image view displayed on first item of the list
    
    * image view displayed on first item of the list
    
    * need to set holder for a image - no idea how
    
    * Book list with dummy data
    
    * refactored code
    
    * [#8] Add gitignore
    
    * [#8] Use tools namespoce to clear warnings
    
    * [#8] Add plan for an android test
    
    * [#8] Write test which checks number of books
    
    * [#8] Test if Recycler View Item has adapter and it's set
    
    * [#8] Test if first item in list has title and author
    
    * [#8] No need for 2 LinearLayouts
    
    * [#8] Rename variables to be more accurate
    
    * [#8]  Use data binding library
    
    * [#8] Rename variables and delete unnecessary code
    
    * [#8] Weird design but working with data binding
    
    * [#8] Weird display and no picture displayed
    
    * [#8] List view will display all books with image using data binding
    
    * [#8] Tests run correctly
    
    * [#8] Split test
    
     Check separately if book has author and if book has title

---
+++ app/src/androidTest/java/com/example/alexandra/bigriverbookstore/MainActivityTest.kt
@@ -0,0 +1,49 @@
+package com.example.alexandra.bigriverbookstore
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import com.example.alexandra.bigriverbookstore.CustomAssertions.Companion.hasItemCount
+import com.example.alexandra.bigriverbookstore.CustomMatchers.Companion.withItemCount
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+    @Rule
+    @JvmField
+    var activityRule = ActivityTestRule<MainActivity>(
+        MainActivity::class.java
+    )
+
+    @Test
+    fun countBooks() {
+        onView(withId(R.id.book_list))
+            .check(matches(withItemCount(6)))
+    }
+
+    @Test
+    fun countBooksWithViewAssertion() {
+        onView(withId(R.id.book_list))
+            .check(hasItemCount(6))
+    }
+
+    @Test
+    fun checkIfFirstItemHasAuthorName() {
+        onView(
+            RecyclerViewMatcher(R.id.book_list)
+                .atPositionOnView(0, R.id.authorName)
+        )
+            .check(matches(withText("Paul k")))
+    }
+
+    @Test
+    fun checkIfFirstItemHasBookTitle() {
+        onView(
+            RecyclerViewMatcher(R.id.book_list)
+                .atPositionOnView(0, R.id.bookTitle)
+        )
+            .check(matches(withText("book t1")))
+    }
+}
---
+++ app/src/main/java/com/example/alexandra/bigriverbookstore/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.alexandra.bigriverbookstore
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recycleView = findViewById<RecyclerView>(R.id.book_list)
+        setRecycleView(recycleView)
+    }
+
+    private fun setRecycleView(recyclerView: RecyclerView) {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val books = ArrayList<Book>()
+        books.add(
+            Book(
+                "Paul k",
+                "book t1",
+                "https://lorempixel.com//400//300//abstract//Faker//?50138"
+            )
+        )
+        books.add(
+            Book(
+                "Jane p",
+                "book t2",
+                "https://lorempixel.com//400//300//abstract//Faker//?50139"
+            )
+        )
+        books.add(
+            Book(
+                "John d",
+                "book t3",
+                "https://lorempixel.com//400//300//abstract//Faker//?50138"
+            )
+        )
+        books.add(
+            Book(
+                "Amy f",
+                "book t4",
+                "https://lorempixel.com//400//300//abstract//Faker//?50138"
+            )
+        )
+        books.add(
+            Book(
+                "Amy f",
+                "book t5",
+                "https://lorempixel.com//400//300//abstract//Faker//?50138"
+            )
+        )
+        books.add(
+            Book(
+                "Amy f",
+                "book t6",
+                "https://lorempixel.com//400//300//abstract//Faker//?50138"
+            )
+        )
+        recyclerView.adapter = BookAdapter(books)
+    }
+}
+
